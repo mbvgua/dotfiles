04c19170ef92eba37f0460a96f3b53206e0be360
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-04-21 06:56:01 +0300

    Finally decided to create dotfiles!!
    - Recently began being a vim power(ish) user and decided to create
      dotfiles to help me along the journey.
    - After doing some reading on the issue, realized i could also create
      dotfiles for other commonly used tools, git and bash, and so I did
      that
    - Looking forward to seeing this grow gradually

---
+++ install.sh
@@ -0,0 +1,23 @@
+#install the tools
+# git
+# bash
+# vim
+
+# create the directory
+```
+    $ cd ~
+    $ mkdir .dotfiles
+    $ cd .dotfiles
+```
+
+# create the subdirectories
+```
+    $ mkdir bash vim git
+```
+
+#symbolicly link files to each other
+```
+    $ ln -s ~/.vimrc vim/.vimrc
+    $ ln -s ~/.bashrc bash/.bashrc
+    $ ln -s ~/.gitconfig git/.gitconfig
+```
