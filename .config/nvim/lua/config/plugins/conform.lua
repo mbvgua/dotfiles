82b3d02f637db57cbefd5ae3f036a5f914f67054
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-06-06 08:07:43 +0300

    Add formatting on save capabilities
    Installed and configured conform.nvim, which is an all in one formatting
    tool. it uses the various code fomratting options for specific langs and
    merges them into one, where you can format with the use of a key binding
    or on save. Pretty nifty.

---
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,32 @@
+-- manage all your code formatters from a single place, e.g prettier,black
+
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		-- format on save automatically
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+		-- installed formatters
+		formatters_by_ft = {
+			-- frontend
+			html = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			-- frameworks
+			angular = { "prettierd", "prettier", stop_after_first = true },
+			vue = { "prettierd", "prettier", stop_after_first = true },
+			-- langs
+			lua = { "stylua" },
+			python = { "black" },
+			sql = { "sqlfmt" },
+		},
+	},
+	-- format using key bindings
+	vim.keymap.set("n", "<leader>cf", function()
+		require("conform").format({ lsp_format = "fallback" })
+	end, { desc = "[c]ode [f]ormat" }),
+}
--- .config/nvim/lua/config/plugins/statusline.lua
+++ .config/nvim/lua/config/plugins/statusline.lua
@@ -1,80 +1,86 @@
+-- setup the statusline and bufferline to be more informative and prettier
+
 return {
-  'nvim-lualine/lualine.nvim',
-  event = 'VeryLazy',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  opts = {
-    options = {
-      icons_enabled = true,
-      section_separators = { left = '', right = '' },
-      component_separators = { left = '', right = '' },
-      always_divide_middle = true,
-      always_show_tabline = true,
-      -- only 1 statusline for all windows/buffers
-      globalstatus = true
-    },
-    sections = {
-      lualine_a = { { 'mode', separator = { left = '', right = '' } } },
-      lualine_b = {
-        { 'branch', icon = '' },
-        {
-          'diff', symbols = { added = ' ', modified = ' ', removed = ' ' }
-        },
-      },
-      lualine_c = {
-        { 'diagnostics' },
-        {
-          'filename',
-          newfile_status = true,
-          symbols = {
-            modified = '', readonly = '', unnamed = '', newfile = ''
-          },
-          -- values: 0,1,2,3,4
-          path = 4,
-        },
-      },
-      -- TODO: add the LSP ICON here instead of search count
-      lualine_x = {
-         {
-            'lsp_status',
-            icon = '', -- f013
-            symbols = {
-                -- Standard unicode symbols to cycle through for LSP progress:
-                spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
-                -- Standard unicode symbol for when LSP is done:
-                done = '✓',
-                -- Delimiter inserted between LSP names:
-                separator = ' ',
-            },
-            -- List of LSP names to ignore (e.g., `null-ls`):
-            ignore_lsp = {},
-         }
-      },
-      lualine_y = {
-        { 'filetype', icon = { align = 'right' } },
-        'fileformat',
-      },
-      lualine_z = {
-        {
-          -- show line of code in file, like Vim. but proved quite hard
-          -- TODO: make this more readable later on, quite hard
-          -- still works like: '%l/%L' in vim statusline
-          -- . -> current line no. $ -> total lines
-          function()
-            return 'LOC: ' .. vim.fn.line('.') .. '/' .. vim.fn.line('$')
-          end,
-        },
-        { 'progress', separator = { left = '', right = '' } }
-      }
-    },
-    tabline ={
-      -- show open buffers
-      lualine_a = {'buffers'},
-      lualine_b = {''},       
-      lualine_c = {''},     
-      lualine_x = {},
-      lualine_y = {},
-      -- show open tabs
-      lualine_z = {'tabs'}
-    },
-  }
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			icons_enabled = true,
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			always_divide_middle = true,
+			always_show_tabline = true,
+			-- only 1 statusline for all windows/buffers
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = { { "mode", separator = { left = "", right = "" } } },
+			lualine_b = {
+				{ "branch", icon = "" },
+				{
+					"diff",
+					symbols = { added = " ", modified = " ", removed = " " },
+				},
+			},
+			lualine_c = {
+				{ "diagnostics" },
+				{
+					"filename",
+					newfile_status = true,
+					symbols = {
+						modified = "",
+						readonly = "",
+						unnamed = "",
+						newfile = "",
+					},
+					-- values: 0,1,2,3,4
+					path = 4,
+				},
+			},
+			-- TODO: add the LSP ICON here instead of search count
+			lualine_x = {
+				{
+					"lsp_status",
+					icon = "", -- f013
+					symbols = {
+						-- Standard unicode symbols to cycle through for LSP progress:
+						spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+						-- Standard unicode symbol for when LSP is done:
+						done = "✓",
+						-- Delimiter inserted between LSP names:
+						separator = " ",
+					},
+					-- List of LSP names to ignore (e.g., `null-ls`):
+					ignore_lsp = {},
+				},
+			},
+			lualine_y = {
+				{ "filetype", icon = { align = "right" } },
+				"fileformat",
+			},
+			lualine_z = {
+				{
+					-- show line of code in file, like Vim. but proved quite hard
+					-- TODO: make this more readable later on, quite hard
+					-- still works like: '%l/%L' in vim statusline
+					-- . -> current line no. $ -> total lines
+					function()
+						return "LOC: " .. vim.fn.line(".") .. "/" .. vim.fn.line("$")
+					end,
+				},
+				{ "progress", separator = { left = "", right = "" } },
+			},
+		},
+		tabline = {
+			-- show open buffers
+			lualine_a = { "buffers" },
+			lualine_b = { "" },
+			lualine_c = { "" },
+			lualine_x = {},
+			lualine_y = {},
+			-- show open tabs
+			lualine_z = { "tabs" },
+		},
+	},
 }
