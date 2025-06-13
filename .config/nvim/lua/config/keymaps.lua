ade1d540e533292e57ec7cfcbe1fd6908ed9d45b
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-06-13 15:10:02 +0300

    Began using kulala as my Rest client
    No need to go back to Vs-code, pretty easy and I remapped the default
    keyword to <CR> so its much easier to get my responses now.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -66,3 +66,6 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- easily navigate modes in terminal
+vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
