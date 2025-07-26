eec36a5011125ecb6646585c8f058a4bfb74d51a
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-26 16:50:07 +0300

    refactor(config/wezterm): restructure wezterm config
    - modularized weterms configuration. It became much more easier to
      understand and configure on the fly
    - added workspaces, which allow you to fly through your project
      directories. amazing really
    
    - Greatly assisted by the following resources:
    this blog [blog](https://alexplescan.com/posts/2024/08/10/wezterm/)
    and its [gist](https://gist.github.com/alexpls/83d7af23426c8928402d6d79e72f9401)

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -1,21 +1,13 @@
 -- We almost always start by importing the wezterm module
-local wezterm = require 'wezterm'
--- Define a lua table to hold _our_ module's functions
-local module = {}
+local wezterm = require("wezterm")
 
--- Returns a bool based on whether the host operating system's
--- appearance is light or dark.
+local module = {}
 function module.is_dark()
-  -- wezterm.gui is not always available, depending on what
-  -- environment wezterm is operating in. Just return true
-  -- if it's not defined.
-  if wezterm.gui then
-    -- Some systems report appearance like "Dark High Contrast"
-    -- so let's just look for the string "Dark" and if we find
-    -- it assume appearance is dark.
-    return wezterm.gui.get_appearance():find("Dark")
-  end
-  return true
+	-- return true if wezterm.gui is not defined
+	if wezterm.gui then
+		return wezterm.gui.get_appearance():find("Dark")
+	end
+	return true
 end
 
 return module
