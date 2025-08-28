f237bf7a1f501e42ce718b3172234f50fadafed3
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-28 16:04:13 +0300

    feat(config/nvim): add the render markdown plugin
    - add the render markdown plugin that allows easy viewing of markdown
      file from within neovim.
    - edit the config files to promote DRY code, such as local variables in
      beginning of files, e.g o for vim.opt e.t.c

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -4,6 +4,8 @@ local vim = vim or {}
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local hl = vim.api.nvim_set_hl
+local bo = vim.bo
+local o = vim.o
 
 -- Highlight text for some time after yanking
 autocmd("TextYankPost", {
@@ -19,7 +21,7 @@ autocmd("TextYankPost", {
 autocmd("BufWritePre", {
 	callback = function()
 		local extension = "~" .. vim.fn.strftime("%Y-%m-%d-%H%M%S")
-		vim.o.backupext = extension
+		o.backupext = extension
 	end,
 })
 
@@ -27,14 +29,14 @@ autocmd("BufWritePre", {
 autocmd({ "BufNewFile", "BufRead" }, {
 	pattern = "*.py",
 	callback = function()
-		vim.bo.tabstop = 4 -- TAB will be equal to 4 spaces
-		vim.bo.softtabstop = 4 --insert/delete 4 spaces when hitting TAB/BACKSPACE
-		vim.bo.shiftwidth = 4 -- operation >> & << will indent/unindent 4 columns
-		vim.bo.textwidth = 79 -- lines longer than 79 chars are broken
-		vim.bo.colorcolumn = "80" -- where right horizontal line margin will be showed
-		vim.bo.expandtab = true -- inser spaces when hitting TAB
-		vim.bo.autoindent = true -- align new line indentation with previous line
-		vim.bo.fileformat = "unix" -- format all files to have unix base EOF
+		bo.tabstop = 4 -- TAB will be equal to 4 spaces
+		bo.softtabstop = 4 --insert/delete 4 spaces when hitting TAB/BACKSPACE
+		bo.shiftwidth = 4 -- operation >> & << will indent/unindent 4 columns
+		bo.textwidth = 79 -- lines longer than 79 chars are broken
+		bo.colorcolumn = 80 -- where right horizontal line margin will be showed
+		bo.expandtab = true -- inser spaces when hitting TAB
+		bo.autoindent = true -- align new line indentation with previous line
+		bo.fileformat = "unix" -- format all files to have unix base EOF
 	end,
 })
 
