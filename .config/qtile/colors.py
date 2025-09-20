a0be721713a9cf5d219bbb9b8b7ed885772a2845
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-09-21 02:06:47 +0300

    refactor(qtile): make it better aesthetically
    - added some more qtile functionality, while also significantly
      improving its UI outlook.
    - Noted some areas that qtile needs improvement on, if there are no OS
      libararies handling it yet:
        - Eye protection(My Eyeesss!!)
        - Accessibility(I need larger fonts in apps)
        - Airplane Mode/DND
        - Wifi connection(cant do this over the terminal dude)
        - Bluetooth connection
        - Volume controll
        - Lock screen!? Currently using gnome partition for that
        - Calender. Desktop Icons

--- .config/qtile/colors.py
+++ .config/qtile/colors.py
@@ -1,132 +1,208 @@
-# If using transparency, make sure you add (background="#00000000") to 'Screen' line(s).
-# Then, you can use RGBA color codes to add transparency to the colors below.
-# For ex: colors = [["#282c34ee", "#282c34dd"], ...
+# DoomOne
+def doomone():
+    colors = [
+        ["#282c34", "#282c34"],  # bg
+        ["#bbc2cf", "#bbc2cf"],  # fg
+        ["#1c1f24", "#1c1f24"],  # color01
+        ["#ff6c6b", "#ff6c6b"],  # color02
+        ["#98be65", "#98be65"],  # color03
+        ["#da8548", "#da8548"],  # color04
+        ["#51afef", "#51afef"],  # color05
+        ["#c678dd", "#c678dd"],  # color06
+        ["#46d9ff", "#46d9ff"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
+    ]
+    backgroundColor = "#282c34"
+    foregroundColor = "#bbc2cf"
+    workspaceColor = "#51afef"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
 
-DoomOne = [
-    ["#282c34", "#282c34"], # bg
-    ["#bbc2cf", "#bbc2cf"], # fg
-    ["#1c1f24", "#1c1f24"], # color01
-    ["#ff6c6b", "#ff6c6b"], # color02
-    ["#98be65", "#98be65"], # color03
-    ["#da8548", "#da8548"], # color04
-    ["#51afef", "#51afef"], # color05
-    ["#c678dd", "#c678dd"], # color06
-    ["#46d9ff", "#46d9ff"], # color15
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
-    ]
 
-Dracula  = [
-    ["#282a36", "#282a36"], # bg
-    ["#f8f8f2", "#f8f8f2"], # fg
-    ["#000000", "#000000"], # color01
-    ["#ff5555", "#ff5555"], # color02
-    ["#50fa7b", "#50fa7b"], # color03
-    ["#f1fa8c", "#f1fa8c"], # color04
-    ["#bd93f9", "#bd93f9"], # color05
-    ["#ff79c6", "#ff79c6"], # color06
-    ["#9aedfe", "#9aedfe"], # color15
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
+# Dracula
+def dracula():
+    colors = [
+        ["#282a36", "#282a36"],  # bg
+        ["#f8f8f2", "#f8f8f2"],  # fg
+        ["#000000", "#000000"],  # color01
+        ["#ff5555", "#ff5555"],  # color02
+        ["#50fa7b", "#50fa7b"],  # color03
+        ["#f1fa8c", "#f1fa8c"],  # color04
+        ["#bd93f9", "#bd93f9"],  # color05
+        ["#ff79c6", "#ff79c6"],  # color06
+        ["#9aedfe", "#9aedfe"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
     ]
+    backgroundColor = "#282a36"
+    foregroundColor = "#f8f8f2"
+    workspaceColor = "#bd93f9"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
 
-GruvboxDark  = [
-    ["#282828", "#282828"], # color[0]
-    ["#ebdbb2", "#ebdbb2"], # color[1]
-    ["#000000", "#000000"], # color[2]
-    ["#fb4934", "#fb4934"], # color[3]
-    ["#98971a", "#98971a"], # color[4]
-    ["#d79921", "#d79921"], # color[5]
-    ["#83a598", "#83a598"], # color[6]
-    ["#d3869b", "#d3869b"], # color[7]
-    ["#b8bb26", "#b8bb26"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
-    ]
-MonokaiPro = [
-    ["#2D2A2E", "#2D2A2E"], # color[0]
-    ["#FCFCFA", "#FCFCFA"], # color[1]
-    ["#403E41", "#403E41"], # color[2]
-    ["#FF6188", "#FF6188"], # color[3]
-    ["#A9DC76", "#A9DC76"], # color[4]
-    ["#FFD866", "#FFD866"], # color[5]
-    ["#FC9867", "#FC9867"], # color[6]
-    ["#AB9DF2", "#AB9DF2"], # color[7]
-    ["#78DCE8", "#78DCE8"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
-    ]
 
-Nord = [
-    ["#2E3440", "#2E3440"], # color[0]
-    ["#D8DEE9", "#D8DEE9"], # color[1]
-    ["#3B4252", "#3B4252"], # color[2]
-    ["#BF616A", "#BF616A"], # color[3]
-    ["#A3BE8C", "#A3BE8C"], # color[4]
-    ["#EBCB8B", "#EBCB8B"], # color[5]
-    ["#81A1C1", "#81A1C1"], # color[6]
-    ["#B48EAD", "#B48EAD"], # color[7]
-    ["#88C0D0", "#88C0D0"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
+# Gruvbox Dark
+def gruvbox_dark():
+    colors = [
+        ["#282828", "#282828"],  # bg
+        ["#ebdbb2", "#ebdbb2"],  # fg
+        ["#000000", "#000000"],  # color01
+        ["#fb4934", "#fb4934"],  # color02
+        ["#98971a", "#98971a"],  # color03
+        ["#d79921", "#d79921"],  # color04
+        ["#83a598", "#83a598"],  # color05
+        ["#d3869b", "#d3869b"],  # color06
+        ["#b8bb26", "#b8bb26"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
     ]
+    backgroundColor = "#282828"
+    foregroundColor = "#ebdbb2"
+    workspaceColor = "#d79921"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
 
-OceanicNext = [
-    ["#1b2b34", "#1b2b34"], # color[0]
-    ["#d8dee9", "#d8dee9"], # color[1]
-    ["#29414f", "#29414f"], # color[2]
-    ["#ec5f67", "#ec5f67"], # color[3]
-    ["#99c794", "#99c794"], # color[4]
-    ["#fac863", "#fac863"], # color[5]
-    ["#6699cc", "#6699cc"], # color[6]
-    ["#c594c5", "#c594c5"], # color[7]
-    ["#5fb3b3", "#5fb3b3"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
-    ]
 
-Palenight = [
-    ["#292d3e", "#292d3e"], # color[0]
-    ["#d0d0d0", "#d0d0d0"], # color[1]
-    ["#434758", "#434758"], # color[2]
-    ["#f07178", "#f07178"], # color[3]
-    ["#c3e88d", "#c3e88d"], # color[4]
-    ["#ffcb6b", "#ffcb6b"], # color[5]
-    ["#82aaff", "#82aaff"], # color[6]
-    ["#c792ea", "#c792ea"], # color[7]
-    ["#89ddff", "#89ddff"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
+# Monokai Pro
+def monokai_pro():
+    colors = [
+        ["#2D2A2E", "#2D2A2E"],  # bg
+        ["#FCFCFA", "#FCFCFA"],  # fg
+        ["#403E41", "#403E41"],  # color01
+        ["#FF6188", "#FF6188"],  # color02
+        ["#A9DC76", "#A9DC76"],  # color03
+        ["#FFD866", "#FFD866"],  # color04
+        ["#FC9867", "#FC9867"],  # color05
+        ["#AB9DF2", "#AB9DF2"],  # color06
+        ["#78DCE8", "#78DCE8"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
     ]
+    backgroundColor = "#2D2A2E"
+    foregroundColor = "#FCFCFA"
+    workspaceColor = "#FF6188"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
 
-SolarizedDark = [
-    ["#002b36", "#002b36"], # color[0]
-    ["#839496", "#839496"], # color[1]
-    ["#073642", "#073642"], # color[2]
-    ["#dc322f", "#dc322f"], # color[3]
-    ["#859900", "#859900"], # color[4]
-    ["#b58900", "#b58900"], # color[5]
-    ["#268bd2", "#268bd2"], # color[6]
-    ["#d33682", "#d33682"], # color[7]
-    ["#2aa198", "#2aa198"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
-    ]
 
-SolarizedLight = [
-    ["#fdf6e3", "#fdf6e3"], # color[0]
-    ["#657b83", "#657b83"], # color[1]
-    ["#ece5ac", "#ece5ac"], # color[2]
-    ["#dc322f", "#dc322f"], # color[3]
-    ["#859900", "#859900"], # color[4]
-    ["#b58900", "#b58900"], # color[5]
-    ["#268bd2", "#268bd2"], # color[6]
-    ["#d33682", "#d33682"], # color[7]
-    ["#2aa198", "#2aa198"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
+# Nord
+def nord():
+    colors = [
+        ["#2E3440", "#2E3440"],  # bg
+        ["#D8DEE9", "#D8DEE9"],  # fg
+        ["#3B4252", "#3B4252"],  # color01
+        ["#BF616A", "#BF616A"],  # color02
+        ["#A3BE8C", "#A3BE8C"],  # color03
+        ["#EBCB8B", "#EBCB8B"],  # color04
+        ["#81A1C1", "#81A1C1"],  # color05
+        ["#B48EAD", "#B48EAD"],  # color06
+        ["#88C0D0", "#88C0D0"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
     ]
+    backgroundColor = "#2E3440"
+    foregroundColor = "#D8DEE9"
+    workspaceColor = "#81A1C1"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
 
-TomorrowNight = [
-    ["#1d1f21", "#1d1f21"], # color[0]
-    ["#c5c8c6", "#c5c8c6"], # color[1]
-    ["#373b41", "#373b41"], # color[2]
-    ["#cc6666", "#cc6666"], # color[3]
-    ["#b5bd68", "#b5bd68"], # color[4]
-    ["#e6c547", "#e6c547"], # color[5]
-    ["#81a2be", "#81a2be"], # color[6]
-    ["#b294bb", "#b294bb"], # color[7]
-    ["#70c0ba", "#70c0ba"], # color[8]
-    ["#7d7d7d", "#7d7d7d"]  # color[9]
+
+# Oceanic Next
+def oceanic_next():
+    colors = [
+        ["#1b2b34", "#1b2b34"],  # bg
+        ["#d8dee9", "#d8dee9"],  # fg
+        ["#29414f", "#29414f"],  # color01
+        ["#ec5f67", "#ec5f67"],  # color02
+        ["#99c794", "#99c794"],  # color03
+        ["#fac863", "#fac863"],  # color04
+        ["#6699cc", "#6699cc"],  # color05
+        ["#c594c5", "#c594c5"],  # color06
+        ["#5fb3b3", "#5fb3b3"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
     ]
+    backgroundColor = "#1b2b34"
+    foregroundColor = "#d8dee9"
+    workspaceColor = "#6699cc"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
+
+
+# Palenight
+def palenight():
+    colors = [
+        ["#292d3e", "#292d3e"],  # bg
+        ["#d0d0d0", "#d0d0d0"],  # fg
+        ["#434758", "#434758"],  # color01
+        ["#f07178", "#f07178"],  # color02
+        ["#c3e88d", "#c3e88d"],  # color03
+        ["#ffcb6b", "#ffcb6b"],  # color04
+        ["#82aaff", "#82aaff"],  # color05
+        ["#c792ea", "#c792ea"],  # color06
+        ["#89ddff", "#89ddff"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
+    ]
+    backgroundColor = "#292d3e"
+    foregroundColor = "#d0d0d0"
+    workspaceColor = "#82aaff"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
+
+
+# Solarized Dark
+def solarized_dark():
+    colors = [
+        ["#002b36", "#002b36"],  # bg
+        ["#839496", "#839496"],  # fg
+        ["#073642", "#073642"],  # color01
+        ["#dc322f", "#dc322f"],  # color02
+        ["#859900", "#859900"],  # color03
+        ["#b58900", "#b58900"],  # color04
+        ["#268bd2", "#268bd2"],  # color05
+        ["#d33682", "#d33682"],  # color06
+        ["#2aa198", "#2aa198"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
+    ]
+    backgroundColor = "#002b36"
+    foregroundColor = "#839496"
+    workspaceColor = "#268bd2"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
+
+
+# Solarized Light
+def solarized_light():
+    colors = [
+        ["#fdf6e3", "#fdf6e3"],  # bg
+        ["#657b83", "#657b83"],  # fg
+        ["#ece5ac", "#ece5ac"],  # color01
+        ["#dc322f", "#dc322f"],  # color02
+        ["#859900", "#859900"],  # color03
+        ["#b58900", "#b58900"],  # color04
+        ["#268bd2", "#268bd2"],  # color05
+        ["#d33682", "#d33682"],  # color06
+        ["#2aa198", "#2aa198"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
+    ]
+    backgroundColor = "#fdf6e3"
+    foregroundColor = "#657b83"
+    workspaceColor = "#268bd2"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
+
+
+# Tomorrow Night
+def tomorrow_night():
+    colors = [
+        ["#1d1f21", "#1d1f21"],  # bg
+        ["#c5c8c6", "#c5c8c6"],  # fg
+        ["#373b41", "#373b41"],  # color01
+        ["#cc6666", "#cc6666"],  # color02
+        ["#b5bd68", "#b5bd68"],  # color03
+        ["#e6c547", "#e6c547"],  # color04
+        ["#81a2be", "#81a2be"],  # color05
+        ["#b294bb", "#b294bb"],  # color06
+        ["#70c0ba", "#70c0ba"],  # color15
+        ["#7d7d7d", "#7d7d7d"],  # color09
+    ]
+    backgroundColor = "#1d1f21"
+    foregroundColor = "#c5c8c6"
+    workspaceColor = "#81a2be"
+    foregroundColorTwo = "#7d7d7d"
+    return colors, backgroundColor, foregroundColor, workspaceColor, foregroundColorTwo
