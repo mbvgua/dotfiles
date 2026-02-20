6b343bfc87539fb171e0227f49f244be4fd72e9f
mbvgua <105845499+mbvgua@users.noreply.github.com>
2026-02-20 23:46:13 +0300

    feat: mlue light filter
    - figured out how to have a sort of standard.ish mode in both my
      coloured and monochromed instances. also im now leaning towards the
      monochrome sode, so made that have bluelight filters without redshift,
      due some unprecented collisions
    - the qute browsers bookmarks grow even longer

--- .config/picom/grayscale.glsl
+++ .config/picom/grayscale.glsl
@@ -13,33 +13,13 @@ vec4 window_shader() {
     float gray = (color.r * 0.299) + (color.g * 0.587) + (color.b * 0.114);
 
     // 2. Apply "Warmth" (Blue Light Filter)
-    // We reduce Blue and slightly increase Red to mimic a 3500K-4500K temperature
+    // For 3400K: R=1.0, G=0.82, B=0.59
+    // (For 5700K: R=1.0, G=0.96, B=0.91)
     vec3 warm_gray = vec3(
-        gray * 1.1,  // Slight boost to red
-        gray * 0.95, // Neutral green
-        gray * 0.7   // Significantly reduced blue
+        gray * 1.20,  // Slight boost to red
+        gray * 0.80, // Neutral green
+        gray * 0.50   // Significantly reduced blue
     );
 
     return vec4(warm_gray * opacity, color.a * opacity);
 }
-
-
-// no inbuilt bluelight filter yet redshift is deactivated hence used above one
-// #version 330
-//
-// in vec2 texcoord;             
-// uniform sampler2D tex;        
-// uniform float opacity;        
-//
-// vec4 window_shader() {
-//     vec2 texsize = textureSize(tex, 0);
-//     vec4 color = texture2D(tex, texcoord / texsize, 0);
-//
-//     // Using a simpler conversion math that handles float precision better
-//     float gray = (color.r * 0.299) + (color.g * 0.587) + (color.b * 0.114);
-//
-//     // Construct the output carefully
-//     vec3 grayscale = vec3(gray);
-//
-//     return vec4(grayscale * opacity, color.a * opacity);
-// }
