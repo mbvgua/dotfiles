c8577243f1f8403b0e98a9d72c3cf3192367906e
mbvgua <105845499+mbvgua@users.noreply.github.com>
2026-02-22 13:56:58 +0300

    feat(picom): made grayscale work with redshift
    - made the grayscale glsl filter work with the redshist based blue light
      filter instead of having one added in the config. this is pretty
      decent and i experience no more tearing on switching themes anymore.
    - removed all wezterm keybinds, since Ctrl+W is needed to switch windows
      in vim. now using wezterm like a basic dumb terminal, with most of the
      multipexing and window pane management left to tmux. Do one thing well
      i guess
    - also added my library from cmus, figured it would be neat if I had my
      playlist from wherever, though I cant technically play any songs on
      there
    
    NOTE:
    - this is where I noticed ive spent too many hours on here{174hrs
      58mins, thans Wakatime :(}. With this commit I intend not to push
      anything new on here for a long long while, since my dots are stable
      and work as needed. Only minor improvements can be achieved at this
      point, which dont have that much of ROI!

--- .config/picom/grayscale.glsl
+++ .config/picom/grayscale.glsl
@@ -1,9 +1,9 @@
 //NOTE: version with bluelight filter inbuilt
 #version 330
 
-in vec2 texcoord;             
-uniform sampler2D tex;        
-uniform float opacity;        
+in vec2 texcoord;
+uniform sampler2D tex;
+uniform float opacity;
 
 vec4 window_shader() {
     vec2 texsize = textureSize(tex, 0);
@@ -12,14 +12,7 @@ vec4 window_shader() {
     // 1. Standard Grayscale conversion
     float gray = (color.r * 0.299) + (color.g * 0.587) + (color.b * 0.114);
 
-    // 2. Apply "Warmth" (Blue Light Filter)
-    // For 3400K: R=1.0, G=0.82, B=0.59
-    // (For 5700K: R=1.0, G=0.96, B=0.91)
-    vec3 warm_gray = vec3(
-        gray * 1.20,  // Slight boost to red
-        gray * 0.80, // Neutral green
-        gray * 0.50   // Significantly reduced blue
-    );
+    vec3 grayscale = vec3(gray);
 
-    return vec4(warm_gray * opacity, color.a * opacity);
+    return vec4(grayscale * opacity, color.a * opacity);
 }
