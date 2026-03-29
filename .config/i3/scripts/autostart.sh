6dbfb874b5471ffa308b3d83415e47389ffb59b4
mbvgua <105845499+mbvgua@users.noreply.github.com>
2026-03-29 19:35:59 +0300

    fix: fix volume mute icon
    CHANGES:
    - the volume mute icons was not working as needed, fixed that
    - returni3 config. pretty buggy and has tearing in some cases, but its
      really lightweight, acant argue withthat. will try using it for a
      month max and see how it is. if not onthe same level as qtile, will
      move back. but for now i got scratchpads working with tdrop and its
      not that bad
    
    CONSIDERATIONS:
    - had wanted to remove i3 completely and stick to what I wasusedto, but
      since im trying new things and all, might as well see how it goes!

---
+++ .config/i3/scripts/autostart.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# polkit
+/usr/lib/polkit-1/polkit-agent-helper-1 &
+
+# background
+feh --bg-fill  ~/.dotfiles/wallpapers/ &
+
+# Notifications
+dunst -config ~/.config/dunst/dunstrc &
+
+# network manager
+nm-applet --indicator &
+
+# for audio
+pipewire &
+
+# bluetooth manager
+blueman-applet &
+
+# set my brightness, runit doesnt restore
+brightnessctl set 10% &
+
+# blue light filter
+redshift-gtk &
+
+# battery power management
+xfce4-power-manager &
+
+# lockscreen functionality, turns out xfce is nice!
+xfce4-screensaver &
+
+# mount and unmount drives
+udiskie --automount --notify --smart-tray --file-manager thunar &
