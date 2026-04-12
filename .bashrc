fe6aab88439fd58aa559adab200c3cbb31459be1
mbvgua <mbvgua@gmail.com>
2026-04-12 10:40:12 +0300

    docs(archive): add an archive branch
    - hada git repo that contained my old dotfiles, this duplication is uneccessary
    - opted instead to copyit and create a branch for it here, easier navigation to it anyways.
    - do note that not alot of things worked here, this was what allowed me to learnhow to setup dotfiles and so not alot of things will work.
    - since i cannot retain two .git files, most of the changes have been documented in the CHANGElOG

--- .bashrc
+++ .bashrc
@@ -1,8 +1,21 @@
-# If not running interactively, don't do anything
-[[ $- != *i* ]] && return
+#################################################################
+#Filename: .bashrc                                              #
+#Sections:                                                      #   
+#    1.General...................general bash behaviour         #
+#    2.Aliases....................general aliases               #
+#    3.Packages .................installed packages             #
+#################################################################
 
-# basic fedora-coloured+void-minimalist bash prompt
-export PS1='\[\e[01;32m\][\u@\h \W]\$\[\e[00m\] '
+
+############################################
+# 1.General                                #                     
+############################################
+
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+    . /etc/bashrc
+fi
 
 # User specific environment
 if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
@@ -10,38 +23,35 @@ if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
 fi
 export PATH
 
-# add path to *my* scripts
-export PATH="$PATH:$HOME/.local/scripts"
 
-# set 3.10 as global python verions
-# its of cultural significance whre im from
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
+############################################
+# 2.Aliases                                #                     
+############################################
 
-# pnpm
-export PNPM_HOME="/home/mbugua/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
 
-# load alises and functions
-for file in ~/.{bash_aliases,bash_functions}; do
-    [ -r "$file" ] && [ -f "$file" ] && source "$file"
-done
-unset file
+if [ -d ~/.bashrc.d ]; then
+    for rc in ~/.bashrc.d/*; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+fi
+unset rc
+export PATH="$PATH:/opt/mssql-tools/bin"
+
+
+# downloading entire websites with wget
+alias wget-ds="wget --mirror --convert-links --adjust-extension --page-requisites --no-parent "
+
+
+
+############################################
+# 3.Packages                               #                     
+############################################
+
 
 # Load Angular CLI autocompletion.
 source <(ng completion script)
 
-# open man pages in neovim
-# thanks @mental_outlaw - Yt
-export MANPAGER="nvim +Man!"
-export MANWIDTH="$((COLUMNS > 90 ? 90 : COLUMNS)) man"
-
-# ignore duplicates in history
-export HISTCONTROL=ignoredups:erasedups
-
-#ignore upper and lowercase when TAB completion
-bind "set completion-ignore-case on"
+# Set up fzf key bindings and fuzzy completion
+eval "$(fzf --bash)"
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,87 +1,130 @@
-local vim = vim or {}
+-- LEADER KEY
+-- set the leader key to space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
--- meta accessor for keymap
-local map = vim.keymap.set
 
--- Normal Mode Tricks
--- copy & paste with Shift-(y/p)
-map({ "n", "x" }, "<S-y>", '"+y', { desc = "copy to clipboard" })
-map({ "n", "x" }, "<S-p>", '"+p', { desc = "paste clipboard" })
+-- CMD SHORTCUTS
+-- copy to and from with Shift-(y)
+vim.keymap.set(
+    {'n','x'},
+    '<S-y>','"+y',
+    {desc='copy to clipboard'}
+)
 
--- save current file
-map("n", "<leader>w", "<cmd>write<cr>", { desc = "save file" })
+-- paste to and from with Shift-(p)
+vim.keymap.set(
+    {'n','x'},
+    '<S-p>','"+p',
+    {desc='paste clipboard text'}
+)
 
--- close current window
-map("n", "<leader>x", "<c-w>c", { desc = "close current window" })
+-- save current file quickly
+vim.keymap.set(
+    'n',
+    '<leader>w','<cmd>write<cr>',
+    {desc = 'Save file'}
+)
+
+-- close current window quickly
+vim.keymap.set(
+    'n',
+    '<leader>c','<C-w>c',
+    {desc='Close current window'}
+)
+
+-- close all file and exit vim
+vim.keymap.set(
+    'n', 
+    '<leader>q', 
+    '<cmd>quitall<cr>', 
+    {desc = 'Exit vim'}
+)
 
 -- stop highlighting after search
-map("n", "<leader>so", ":nohlsearch<CR>", { desc = "removing highlighted text after a search" })
+-- (H)ighlight (O)ff
+vim.keymap.set(
+    'n',
+    '<leader>ho',
+    ':nohlsearch<CR>',
+    {desc='Removing highlighted text after a search'}
+)
 
--- check spelling within file
-map("n", "<leader>cs", "<cmd>setlocal spell!<CR>", { desc = "[c]heck [s]pelling" })
+-- easy window navigation with leader key
+-- navigate to left
+vim.keymap.set(
+    'n','<leader>h','<C-w>h',{desc='Navigate to left window'}
+)
 
--- window navigation with leader-(h,l,k,j)
-map("n", "<leader>h", "<C-w>h", { desc = "navigate to left window" })
-map("n", "<leader>l", "<C-w>l", { desc = "navigate to right window" })
-map("n", "<leader>k", "<C-w>k", { desc = "navigate to window above" })
-map("n", "<leader>j", "<C-w>j", { desc = "navigate to window below" })
+-- navigate to right
+vim.keymap.set(
+    'n','<leader>l','<C-w>l',{desc='Navigate to right window'}
+)
 
--- resize windows with Ctrl+h\j\k\l Keys. like tmux
-map("n", "<A-k>", ":resize +2<CR>", { desc = "resize horizontally to be bigger" })
-map("n", "<A-j>", ":resize -2<CR>", { desc = "resize horizontally to be smaller" })
-map("n", "<A-h>", ":vertical resize -2<CR>", { desc = "resize vertically to be smaller" })
-map("n", "<A-l>", ":vertical resize +2<CR>", { desc = "resize vertically to be bigger" })
+-- navigate up
+vim.keymap.set(
+    'n','<leader>k','<C-w>k',{desc='Navigate to window above'}
+)
 
--- navigate buffers easily with leader-(N)ext and (P)revious
-map("n", "<leader>n", ":bnext<CR>", { desc = "navigate to next buffer" })
-map("n", "<leader>p", ":bprevious<CR>", { desc = "navigate to previous buffer" })
+-- navigate down
+vim.keymap.set(
+    'n','<leader>j','<C-w>j',{desc='Navigate to window below'}
+)
+  
+-- resize windows with arrow keys easily
+-- resize upwards
+vim.keymap.set(
+    'n', '<C-Up>', ':resize +2<CR>'
+)
 
--- easily split windows with - & | like tmux
-map("n", "<leader>-",":split<CR>",{desc = "[-]Split window horizontally "})
-map("n", "<leader>\\",":vsplit<CR>",{desc = "[|] Split window vertically "})
+-- resize downwards
+vim.keymap.set(
+    'n', '<C-Down>', ':resize -2<CR>'
+)
 
--- Hardmode ON!! from kickstart nvim
--- Disable arrow keys in normal & visual mode
-map({"v","n"}, "<left>", '<cmd>echohl Error | echo "Youre in Hardmode.Use h to move!!" | echohl None<CR>')
-map({"v","n"}, "<right>", '<cmd>echohl Error | echo "Youre in Hardmode.Use l to move!!" | echohl None<CR>')
-map({"v","n"}, "<up>", '<cmd>echohl Error | echo "Youre in Hardmode.Use k to move!!" | echohl None<CR>')
-map({"v","n"}, "<down>", '<cmd>echohl Error | echo "Youre in Hardmode.Use j to move!!" | echohl None<CR>')
+-- resize to the left
+vim.keymap.set(
+    'n', '<C-Left>', ':vertical resize -2<CR>'
+)
 
--- Visual Mode Tricks
+-- resize to the right
+vim.keymap.set(
+    'n', '<C-Right>', ':vertical resize +2<CR>'
+)
+
+-- navigate buffers easily with leader key
+-- (N)ext and (P)revious
+vim.keymap.set(
+    'n', '<leader>n', ':bnext<CR>'
+)
+
+vim.keymap.set(
+    'n', '<leader>p', ':bprevious<CR>'
+) 
+
+-- visual mode tricks
 -- remain in visual mode while identing to the right/left
-map("v", "<", "<gv", { desc = "ident leftwards" })
-map("v", ">", ">gv", { desc = "ident rightwards" })
+vim.keymap.set(
+    'v', '<', '<gv'
+)
+
+vim.keymap.set(
+    'v', '>', '>gv'
+)
+
+-- Move text up and down
+vim.keymap.set("v", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-k>", ":m .-2<CR>==")
 
 -- Continously move block of text up and down
-map({"v","x"}, "K", ":move '<-2<CR>gv-gv",{ desc = "move chunk upwards" })
-map({"v","x"}, "J", ":move '>+1<CR>gv-gv",{desc="move chunk downwards"} )
-map({"v","x"}, "<A-k>", ":move '<-2<CR>gv-gv",{ desc = "move chunk upwards" })
-map({"v","x"}, "<A-j>", ":move '>+1<CR>gv-gv", { desc = "move chunk downwards" })
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
+vim.keymap.set("x", "<A-j>", ":move '>+1<CR>gv-gv")
+vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
 
--- delete current buffer
-map("n","<leader>bb",":bd<CR>",{desc="[d]elete current [b]uffer"})
-
--- delete all buffers in background
-map("n", "<leader>bd", function()
-	local current = vim.api.nvim_get_current_buf()
-
-	-- First, save all modified buffers
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted and vim.bo[buf].modified then
-			vim.api.nvim_buf_call(buf, function()
-				vim.cmd("write")
-			end)
-		end
-	end
-
-	-- Then, delete all except the current one
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if buf ~= current and vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
-			vim.api.nvim_buf_delete(buf, { force = true })
-		end
-	end
-end, { desc = "[d]elete all [b]uffers except current" })
-
--- insert base html desc
--- Learnt from https://github.com/changemewtf/no_plugins/blob/master/no_plugins.vim
-map({ "n", "x" }, "<leader>!", ":-1read $HOME/.config/nvim/base.html<cR>9jwf>a", { desc = "insert html boilerplate" })
+-- Terminal --
+-- Better terminal navigation
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-w>h")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-w>j")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-w>k")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-w>l")
