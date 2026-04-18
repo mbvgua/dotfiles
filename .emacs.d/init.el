989e92dc40854c10d2c812f0e828e9e4a0eda36b
mbvgua <mbvgua@gmail.com>
2026-04-18 19:09:58 +0300

    feat(emacs): dip my toes into emacs
    CHANGES:
    - decided to get into the lisps, and can only do this withthe emacs
      editors.Tried itout andmight have toadmit that it ispretty powerful,
      moreso in the evaluating files with M-x, that blew my mind!!
    - the ui settings are pretty minimal anduser friendly too, feeling like
      I can replicate my nvim config rathermore easily here, and build it
      for myuse better. will work onthis gradually, no tutorials at all, and
      see what I end up with. want to customize this to belike a second
      brain.
    - also did try out the main two flavorsof emacs, i.e Spacemacs -> was
      too heavy while building, the seems tooclattered, in terms of its UI.
      and Doomemacs-> pretty light while building, but on use, it devoured a
      whooping 200mb of my cpu, thats more than all my other apps
      combined?!?! not doing that again.
    - also feeling that I have been down this rabbit hole before(with
      Kickstarter or Lazy.nvim) I prefer building things from scratch, that
      is the point of customizable editors after all. I dont want to learn
      another persons version of best keybinds...
    
    CONSIDERATIONS:
    - (Neo)Vim has been nice, really nice, but the motions and modal editing
        is what had/ and still peaks my interest. Ig I can replicate this in
        emacs,with evil mode and evil collection, then I think I could live
        in emacs for abit, see whatboth world have to offer :)

---
+++ .emacs.d/init.el
@@ -0,0 +1,45 @@
+;; Install essential packages
+;; Try M-x list-packages to see what's available.
+(require 'package)
+(add-to-list 'package-archives '(("melpa" . "https://melpa.org/packages/")
+                                 ("elpa" . "https://elpa.gnu.org/packages/"))
+                                 t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; The packages you want installed. You can also install these
+;; manually with M-x package-install
+(defvar my-packages
+  '(
+    monokai-pro-theme          ;; monokai theme
+    evil                 ;; vim motions!!!
+    evil-collection
+    )
+  )
+
+;; install the packages
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; enable evil mode :|
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+;; add my customization directory to path
+(add-to-list 'load-path "~/.emacs.d/settings")
+
+(load "ui.el")         ;; change emacs ui
